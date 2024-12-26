cf3c0501ce6a8258ea1cb83b5e4e959301eb2295
Filipe Rocha <filiperochasantos4@gmail.com>
2024-12-26 20:40:58 -0300

    chore: indexation fixed and comments removed

--- migrations/20241226231419-set-active-products.js
+++ migrations/20241226231419-set-active-products.js
@@ -1,19 +1,17 @@
-'use strict';
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Products', 'active', {
+    await queryInterface.addColumn("Products", "active", {
       type: Sequelize.BOOLEAN,
       defaultValue: true,
       allowNull: false,
     });
 
-    // Optional: Set the default value for existing records
-    await queryInterface.sequelize.query('UPDATE Products SET active = true');
+    await queryInterface.sequelize.query("UPDATE Products SET active = true");
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Products', 'active');
+    await queryInterface.removeColumn("Products", "active");
   },
 };
