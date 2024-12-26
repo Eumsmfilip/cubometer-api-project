a2c9b894aa86723a444715ea253a68a0f472a1c1
Filipe Rocha <filiperochasantos4@gmail.com>
2024-12-26 16:47:45 -0300

    fix: excluded database.js from the file property

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
+
 require("dotenv").config();
 
-const client = require("./database/database");
 const port = process.env.APP_PORT;
+const client = require("./models/database");
+const db = require("./models");
+const router = require("./routes/product.route");
+
+app.use(express.json());
+app.use("/api/products", router);
 
 client
   .connect()
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+    db.sequelize.sync().then((req) => {
+      app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
     });
     console.log("Connected to database!");
   })
